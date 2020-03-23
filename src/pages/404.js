bfa8b4f5122c78f625144926706e00e221fe823d
Rychillie Umpierre de Oliveira <rychillie@hotmail.com>
2020-03-23 01:45:51 -0300

    tentando fazer 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,11 @@
 import React from "react"
+
 import Layout from "../components/layout"
 
 const NotFoundPage = () => (
     <Layout>
         <div className={"container"}>
-            <h3>Este é um Error</h3>
+            <h3>Ops deu Ruim</h3>
             <p>Em Construção</p>
         </div>
     </Layout>
---
+++ src/pages/blog.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const BlogPage = () => {
+  return (
+    <Layout>
+      <h3>Este é um Blog</h3>
+      <p>Em Construção</p>
+    </Layout>
+  )
+}
+
+
+export default BlogPage
