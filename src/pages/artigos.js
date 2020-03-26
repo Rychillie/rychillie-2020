de752c002e9da07aed61c1d04ea9cc3206ac7024
Rychillie Umpierre de Oliveira <rychillie@hotmail.com>
2020-03-25 21:25:27 -0300

    some updates

---
+++ src/pages/artigos.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ArtigosPage = () => {
+  return (
+    <Layout>
+      <h3>Este é uma Lista Artigos</h3>
+      <p>Em Construção</p>
+    </Layout>
+  )
+}
+
+
+export default ArtigosPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,28 +8,32 @@ const IndexPage = ({data}) => {
   return (
     <Layout>
       <SEO
-        title="Inicio"
+        title="Pagina Inicial"
       />
       <h3>Ultimas Publicações:</h3>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <Link className={"post"} type={node.frontmatter.categories} key={node.id} to={node.fields.slug} title={node.frontmatter.title}>
-          <div className={"row content"}>
-            <div className={"col-5 imgpost"}>
-              <div className={"imagepost"}>
-                <Img
-                  alt={node.frontmatter.title}
-                  fluid={node.frontmatter.image.childImageSharp.fluid}
-                />
+      <div className={"post-list"}>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <Link className={"post"} type={node.frontmatter.categories} key={node.id} to={node.fields.slug} title={node.frontmatter.title}>
+            <div className={"row content"}>
+              <div className={"col-5 imgpost"}>
+                <div className={"imagepost"}>
+                  <Img
+                    alt={node.frontmatter.title}
+                    fluid={node.frontmatter.image.childImageSharp.fluid}
+                  />
+                </div>
+              </div>
+              <div className={"col-7 details"}>
+                <h3 className={"titlepost"}>{node.frontmatter.title}</h3>
+                <p className={"description"}>{node.frontmatter.description}</p>
+                <span className={"categorypost"}>{node.frontmatter.categories}</span>
+                <span className={"datepost"}>{node.frontmatter.date}</span>
               </div>
             </div>
-            <div className={"col-7 descrition"}>
-              <h3 className={"titlepost"}>{node.frontmatter.title}</h3>
-              <span className={"categorypost"}>{node.frontmatter.categories}</span>
-              <span className={"datepost"}>{node.frontmatter.date}</span>
-            </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
+      </div>
+      <Link className={"btn ver-artigos"} to={"Artigos"}>Todos Artigos</Link>
     </Layout>
   )
 }
@@ -51,6 +55,7 @@ export const pageQuery = graphql`{
             }
           }
           date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+          description
         }
         fields {
           slug
