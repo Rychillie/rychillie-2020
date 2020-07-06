2cb001251add3352af07de48411f2a6f57a540f6
Rychillie <rychillie@hotmail.com>
2020-07-06 13:26:06 -0300

    plugins update

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
     `gatsby-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-netlify`,
     {
       resolve: 'gatsby-transformer-remark',
       options: {
